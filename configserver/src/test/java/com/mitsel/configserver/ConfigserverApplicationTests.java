2bd8985ae6d6847d59552e5e96462a95de8498d1
Mithunkumar <mithun.selvaraj@gmail.com>
2024-12-10 18:41:59 +0530

    configserver on classpath changes

---
+++ configserver/src/test/java/com/mitsel/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitsel.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
