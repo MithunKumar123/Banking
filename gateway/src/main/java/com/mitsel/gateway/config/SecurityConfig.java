cc10dcac185a8c557dab01d37d0d4cfb7928bd84
Mithunkumar <mithun.selvaraj@gmail.com>
2024-12-20 07:54:29 +0530

    Spring security feature is added with keycloak

---
+++ gateway/src/main/java/com/mitsel/gateway/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.mitsel.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
+        serverHttpSecurity
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.GET)
+                        .permitAll()
+                        .pathMatchers("/banking/api/accounts/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/banking/api/cards/**").hasRole("CARDS")
+                        .pathMatchers("/banking/api/loans/**").hasRole("LOANS"))
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(gratedAuthoritiesConverter())));
+
+        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return serverHttpSecurity.build();
+    }
+
+    Converter<Jwt, Mono<AbstractAuthenticationToken>> gratedAuthoritiesConverter(){
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
+}
